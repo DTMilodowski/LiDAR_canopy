4b5e59d68a45e93546fd8b97c018f552a60a1907
David Milodowski <d.t.milodowski@ed.ac.uk>
2017-02-02 12:51:51 +0000

    new driver function exploring improvements to radiative transfer model

---
+++ src/adjusted_radiative_transfer_driver.py
@@ -0,0 +1,75 @@
+import numpy as np
+import sys
+from matplotlib import pyplot as plt
+import LiDAR_tools as lidar
+import auxilliary_functions as aux
+import LiDAR_MacHorn_LAD_profiles as LAD1
+import LiDAR_radiative_transfer_LAD_profiles as LAD2
+
+las_file = 'Carbon_plot_point_cloud_buffer.las'
+subplot_coordinate_file = 'BALI_subplot_coordinates_corrected.csv'
+Plots = ['LF','E','Belian','Seraya','B North','B South','DC1','DC2']
+N_plots = len(Plots)
+leaf_angle_dist = 'spherical'
+max_height = 80
+max_return = 2
+layer_thickness = 1
+n_layers = np.ceil(max_height/layer_thickness)
+minimum_height = 2.
+
+# store profiles in dictionaries
+radiative_spherical_LAD = {}
+radiative_spherical_adjusted_LAD = {}
+lidar_profiles ={}
+
+subplot_polygons, subplot_labels = aux.load_boundaries(subplot_coordinate_file)
+
+all_pts = lidar.load_lidar_data(las_file)
+
+for pp in range(0,N_plots):
+    Plot_name=Plots[pp]
+    n_coord_pairs = subplot_polygons[Plot_name].shape[0]*subplot_polygons[Plot_name].shape[1]
+    coord_pairs = subplot_polygons[Plot_name].reshape(n_coord_pairs,2)
+    bbox_polygon = aux.get_bounding_box(coord_pairs)
+
+    lidar_pts = lidar.filter_lidar_data_by_polygon(all_pts,bbox_polygon)
+
+    n_subplots = subplot_polygons[Plot_name].shape[0]
+    subplot_lidar_profiles = np.zeros((n_subplots,n_layers))
+    subplot_LAD_profiles_spherical = np.zeros((n_subplots,n_layers+1))
+    subplot_LAD_profiles_spherical_adjusted = np.zeros((n_subplots,n_layers+1))
+    n_ground_returns = np.zeros(n_subplots)
+    subplot_LAI = np.zeros(n_subplots)
+
+    heights_rad = np.arange(0,max_height+1)
+
+    for i in range(0,n_subplots):
+        print "Subplot: ", subplot_labels[Plot_name][i]
+        sp_pts = lidar.filter_lidar_data_by_polygon(lidar_pts,subplot_polygons[Plot_name][i,:,:])
+        #print sp_pts[sp_pts[:,3]==1].shape[0]/20./20.
+        heights,subplot_lidar_profiles[i,:],n_ground_returns[i] = LAD1.bin_returns(sp_pts, max_height, layer_thickness)
+
+        u,n,I,U = LAD2.calculate_LAD(sp_pts,heights_rad,max_return,'spherical')
+        subplot_LAD_profiles_spherical[i,:]=u.copy()
+
+        #derive correction factor for number of second returns
+        N_1veg = np.all((sp_pts[:,3]==1,sp_pts[:,4]==1),axis=0).sum()
+        N_2 = (sp_pts[:,3]==2).sum()
+        n[:,:,1]*=N_1veg/N_2
+        u,n,I,U = LAD2.calculate_LAD(sp_pts,heights_rad,max_return,'spherical',n)
+        subplot_LAD_profiles_spherical_adjusted[i,:]=u.copy()
+
+    subplot_LAD_profiles_spherical[np.isnan(subplot_LAD_profiles_spherical)]=0
+    subplot_LAD_profiles_spherical_adjusted[np.isnan(subplot_LAD_profiles_spherical_adjusted)]=0
+
+    # remove all profile values below minimum height prior to comparison
+    #mask = heights <= minimum_height
+    mask = np.max(heights_rad)-heights_rad<=minimum_height
+    subplot_LAD_profiles_spherical[:,mask]=0
+    subplot_LAD_profiles_spherical_adjusted[:,mask]=0
+
+    # store profiles in dictionaries
+
+    lidar_return_profiles[Plot_name] = subplot_lidar_profiles
+    radiative_spherical_LAD[Plot_name] = subplot_LAD_profiles_spherical[:,:-1]
+    radiative_spherical_adjusted_LAD[Plot_name] = subplot_LAD_profiles_spherical_adjusted[:,:-1]

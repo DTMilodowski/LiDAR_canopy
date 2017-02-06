f3810fd85bb508e67fe1aea8f94ccd12399d9e11
David Milodowski <d.t.milodowski@ed.ac.uk>
2017-02-06 09:43:15 +0000

    new code to plot up lidar return profiles and adjusted profiles

--- src/adjusted_radiative_transfer_full_plot_driver.py
+++ src/adjusted_radiative_transfer_full_plot_driver.py
@@ -22,6 +22,7 @@ minimum_height = 2.
 radiative_spherical_LAD = {}
 radiative_spherical_adjusted_LAD = {}
 lidar_profiles ={}
+lidar_profiles_adjusted = {}
 
 #subplot_polygons, subplot_labels = aux.load_boundaries(subplot_coordinate_file)
 subplot_polygons = aux.load_generic_boundaries(coordinate_file)
@@ -33,21 +34,21 @@ for pp in range(0,N_plots):
     print Plot_name
     lidar_pts = lidar.filter_lidar_data_by_polygon(all_pts,bbox_polygon)
 
-    plot_lidar_profiles = np.zeros(n_layers)
+    #plot_lidar_profiles = np.zeros(n_layers)
     LAD_profiles_spherical = np.zeros(n_layers+1)
     LAD_profiles_spherical_adjusted = np.zeros(n_layers+1)
     heights_rad = np.arange(0,max_height+1)
 
-    heights,plot_lidar_profiles,n_ground_returns = LAD1.bin_returns(lidar_pts, max_height, layer_thickness)
     u,n,I,U = LAD2.calculate_LAD(lidar_pts,heights_rad,max_return,'spherical')
     LAD_profiles_spherical=u.copy()
-
+    lidar_profiles[Plot_name] = np.sum(n,axis=1)
     #derive correction factor for number of second returns
     #N_1veg = float(np.all((sp_pts[:,3]==1,sp_pts[:,4]==1),axis=0).sum())
     #N_2 = float((sp_pts[:,3]==2).sum())
     #n[:,:,1]*=N_1veg/N_2
     u,n,I,U = LAD2.calculate_LAD_DTM(lidar_pts,heights_rad,max_return,'spherical')
     LAD_profiles_spherical_adjusted=u.copy()
+    lidar_profiles_adjusted[Plot_name] = np.sum(n,axis=1)
 
     LAD_profiles_spherical[np.isnan(LAD_profiles_spherical)]=0
     LAD_profiles_spherical_adjusted[np.isnan(LAD_profiles_spherical_adjusted)]=0
@@ -69,8 +70,11 @@ for pp in range(0,N_plots):
     ##plt.title(Plot_name)
     # lidar
     ax31 = plt.subplot2grid((1,3),(0,0))
+    colour = ['black','blue','red','orange']
     ax31.annotate('a', xy=(0.05,0.95), xycoords='axes fraction',backgroundcolor='none',horizontalalignment='left', verticalalignment='top', fontsize=10)
-    ax31.plot(lidar_profiles[Plot_name]/1000.,heights,'-',c='k',linewidth=1)
+    for i in range(0,max_return):
+        ax31.plot(lidar_profiles[Plot_name][:,i]/1000.,heights,'-',c=colour[i],linewidth=1)
+        ax31.plot(lidar_profiles_adjusted[Plot_name][:,i]/1000.,heights,'--',c=color[i],linewidth=1)
     ax31.set_ylim(0,80)
     ax31.set_ylabel('Height / m')
     ax31.set_xlabel('Number of returns (x1000)')

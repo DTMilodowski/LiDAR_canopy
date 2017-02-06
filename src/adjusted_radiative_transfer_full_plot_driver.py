2619f172abc8069e25a3a85fa4894e1d2a1f400a
David Milodowski <d.t.milodowski@ed.ac.uk>
2017-02-06 10:33:36 +0000

    new code to plot up transmittance ratio

--- src/adjusted_radiative_transfer_full_plot_driver.py
+++ src/adjusted_radiative_transfer_full_plot_driver.py
@@ -34,21 +34,18 @@ for pp in range(0,N_plots):
     print Plot_name
     lidar_pts = lidar.filter_lidar_data_by_polygon(all_pts,bbox_polygon)
 
-    #plot_lidar_profiles = np.zeros(n_layers)
-    LAD_profiles_spherical = np.zeros(n_layers+1)
-    LAD_profiles_spherical_adjusted = np.zeros(n_layers+1)
     heights_rad = np.arange(0,max_height+1)
 
     u,n,I,U = LAD2.calculate_LAD(lidar_pts,heights_rad,max_return,'spherical')
     LAD_profiles_spherical=u.copy()
-    lidar_profiles[Plot_name] = np.sum(n,axis=1)
+    lidar_profiles[Plot_name] = np.sum(n.copy(),axis=1)
     #derive correction factor for number of second returns
     #N_1veg = float(np.all((sp_pts[:,3]==1,sp_pts[:,4]==1),axis=0).sum())
     #N_2 = float((sp_pts[:,3]==2).sum())
     #n[:,:,1]*=N_1veg/N_2
     u,n,I,U = LAD2.calculate_LAD_DTM(lidar_pts,heights_rad,max_return,'spherical')
     LAD_profiles_spherical_adjusted=u.copy()
-    lidar_profiles_adjusted[Plot_name] = np.sum(n,axis=1)
+    lidar_profiles_adjusted[Plot_name] = np.sum(n.copy(),axis=1)
 
     LAD_profiles_spherical[np.isnan(LAD_profiles_spherical)]=0
     LAD_profiles_spherical_adjusted[np.isnan(LAD_profiles_spherical_adjusted)]=0
@@ -60,50 +57,63 @@ for pp in range(0,N_plots):
     LAD_profiles_spherical_adjusted[mask]=0
 
     # store profiles in dictionaries
-    lidar_profiles[Plot_name] = np.sum(n,axis=1)#plot_lidar_profiles
     radiative_spherical_LAD[Plot_name] = LAD_profiles_spherical[:-1]
     radiative_spherical_adjusted_LAD[Plot_name] = LAD_profiles_spherical_adjusted[:-1]
 
+heights = np.arange(1,81)
 for pp in range(0,N_plots):
     Plot_name=Plots[pp]
     plt.figure(1, facecolor='White',figsize=[10,6.5])
     ##plt.title(Plot_name)
     # lidar
-    ax31 = plt.subplot2grid((1,3),(0,0))
+    ax11 = plt.subplot2grid((1,3),(0,0))
     colour = ['black','blue','red','orange']
-    ax31.annotate('a', xy=(0.05,0.95), xycoords='axes fraction',backgroundcolor='none',horizontalalignment='left', verticalalignment='top', fontsize=10)
+    labels = ['$1^{st}$', '$2^{nd}$', '$3^{rd}$', '$4^{th}$']
+    ax11.annotate('a', xy=(0.05,0.95), xycoords='axes fraction',backgroundcolor='none',horizontalalignment='left', verticalalignment='top', fontsize=10)
     for i in range(0,max_return):
-        ax31.plot(lidar_profiles[Plot_name][:,i]/1000.,heights,'-',c=colour[i],linewidth=1)
-        ax31.plot(lidar_profiles_adjusted[Plot_name][:,i]/1000.,heights,'--',c=color[i],linewidth=1)
-    ax31.set_ylim(0,80)
-    ax31.set_ylabel('Height / m')
-    ax31.set_xlabel('Number of returns (x1000)')
+        ax11.plot(lidar_profiles[Plot_name][:,i]/1000.,np.max(heights_rad)-heights_rad,'-',c=colour[i],linewidth=1,label=labels[i])
+        ax11.plot(lidar_profiles_adjusted[Plot_name][:,i]/1000.,np.max(heights_rad)-heights_rad,'--',c=colour[i],linewidth=1)
+    ax11.set_ylim(0,80)
+    ax11.set_ylabel('Height / m')
+    ax11.set_xlabel('Number of returns (x1000)')
+    ax11.legend(loc=1)
     
     #Radiative Transfer initial
-    ax32 = plt.subplot2grid((1,3),(0,1))
-    ax32.annotate('b', xy=(0.05,0.95), xycoords='axes fraction',backgroundcolor='none',horizontalalignment='left', verticalalignment='top', fontsize=10)
-    ax32.plot(radiative_spherical_LAD[Plot_name],np.max(heights)-heights+1,'-',c='k',linewidth=1)
-    ax32.set_ylim(0,80)
-    ax32.set_xlabel('LAD$_{rad}$ / m$^2$m$^{-1}$')
+    ax12 = plt.subplot2grid((1,3),(0,1))
+    ax12.annotate('b', xy=(0.05,0.95), xycoords='axes fraction',backgroundcolor='none',horizontalalignment='left', verticalalignment='top', fontsize=10)
+    ax12.plot(radiative_spherical_LAD[Plot_name],np.max(heights)-heights+1,'-',c='k',linewidth=1)
+    ax12.set_ylim(0,80)
+    ax12.set_xlabel('LAD$_{rad}$ / m$^2$m$^{-1}$')
 
     #Radiative Transfer adjusted
-    ax33 = plt.subplot2grid((1,3),(0,2),sharex=ax32)
-    ax33.annotate('c', xy=(0.05,0.95), xycoords='axes fraction',backgroundcolor='none',horizontalalignment='left', verticalalignment='top', fontsize=10)
-    ax33.plot(radiative_spherical_adjusted_LAD[Plot_name],np.max(heights)-heights+1,'-',c='k',linewidth=1)
-    ax33.set_ylim(0,80)
-    ax33.set_xlabel('LAD$_{rad}$ / m$^2$m$^{-1}$')
+    ax13 = plt.subplot2grid((1,3),(0,2),sharex=ax12)
+    ax13.annotate(Plot_name, xy=(0.95,0.95), xycoords='axes fraction',backgroundcolor='none',horizontalalignment='right', verticalalignment='top', fontsize=10)
+    ax13.annotate('c', xy=(0.05,0.95), xycoords='axes fraction',backgroundcolor='none',horizontalalignment='left', verticalalignment='top', fontsize=10)
+    ax13.plot(radiative_spherical_adjusted_LAD[Plot_name],np.max(heights)-heights+1,'-',c='k',linewidth=1)
+    ax13.set_ylim(0,80)
+    ax13.set_xlabel('LAD$_{rad}$ / m$^2$m$^{-1}$')
     
 
-    ax32.set_xlim(xmax=0.7)
-    #ax31.set_xlim(xmax=lidar_profiles[Plot_name].max()/1000)
-
-    ax31.locator_params(axis='x',nbins=5)
-    ax32.locator_params(axis='x',nbins=5)
+    ax12.set_xlim(xmax=0.7)
+    
+    ax11.locator_params(axis='x',nbins=5)
+    ax12.locator_params(axis='x',nbins=5)
 
     print Plot_name, radiative_spherical_LAD[Plot_name].sum(),  radiative_spherical_adjusted_LAD[Plot_name].sum()
     plt.tight_layout()
     plt.savefig(Plot_name+'_LAD_radiative_comparison_full_plot_inversion.png')
     plt.show()
 
-# A figure illustrating transmittance factor between successive returns 
+# A figure illustrating transmittance ratio between successive returns 
 plt.figure(2, facecolor='White',figsize=[4,4])
+ax21.set_xlabel('return number')
+ax21.set_ylabel('transmittance ratio')
+for i in range(0,4):
+    if i=1:
+        ax21.plot(1,1,'o',color='blue')
+    else:
+        N_veg = float(np.all((sp_pts[:,3]==i+1,sp_pts[:,4]==1),axis=0).sum())
+        N_i = float((sp_pts[:,3]==i+1).sum())
+        ax21.plot(i+1,N_i/N_veg,'o',color='blue')
+ax21.set_ylim(0,1.1)
+ax21.set_xlim(0,5)

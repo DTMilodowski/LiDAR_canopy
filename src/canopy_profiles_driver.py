ed998ed54893da602d30e1ee8299231dd1c0c5f7
David Milodowski <d.t.milodowski@ed.ac.uk>
2017-02-16 13:55:26 +0000

    adding new driver function to produce first draft of paper figs

---
+++ src/canopy_profiles_driver.py
@@ -0,0 +1,17 @@
+###############################################################################################################
+# This driver function analyses both LiDAR data and field inventory data to produce independent estimates of
+# canopy structure.  These are compared against each other and their integrated LAD is compared against LAI
+# estimates from hemispherical photographs.
+############################################################################################################### 
+import numpy as np
+import sys
+from matplotlib import pyplot as plt
+import LiDAR_tools as lidar
+import auxilliary_functions as aux
+import LiDAR_MacHorn_LAD_profiles as LAD1
+import LiDAR_radiative_transfer_LAD_profiles as LAD2
+import inventory_based_LAD_profiles as field
+
+sys.path.append('/home/dmilodow/DataStore_DTM/BALI/MetDataProcessing/UtilityTools/')
+import statistics_tools as stats
+

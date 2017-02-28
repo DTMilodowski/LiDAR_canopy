36765885bf413888847882d778650899c62f7be7
David Milodowski <d.t.milodowski@ed.ac.uk>
2017-02-28 14:45:50 +0000

    function to calculate subcanopy light transmittance

--- src/canopy_microclimate.py
+++ src/canopy_microclimate.py
@@ -9,3 +9,18 @@ import auxilliary_functions as aux
 import LiDAR_MacHorn_LAD_profiles as LAD1
 import LiDAR_radiative_transfer_LAD_profiles as LAD2
 import inventory_based_LAD_profiles as field
+
+# Estimate distribution of light within canopy with a given LAD, assuming the
+# assumption of horizontal homogeneity.
+# Light transmittance at any point in a continuous vertical column of voxels to
+# a canopy level, j=v, is given by:
+#     I_v = I_0*exp(-k*sum(LAD[j=v:-1]))
+# k is the extinction coefficient defining Beer-Lambert-like decay of light
+# irradiance through the canopy
+# I_0 is the incident radiation at the top of the canopy 
+def estimate_canopy_light_transmittance(LAD,heights,k,I_0):
+    N_levels = heights.size
+    dz = np.abs(heights[1]-heights[0])
+    I = I_0*np.exp( -k*np.cumsum(LAD[::-1])[::-1]*dz )
+
+    return I

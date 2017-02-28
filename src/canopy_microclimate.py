9fc78307e848f061ac29059c3a76f129dba49255
David Milodowski <d.t.milodowski@ed.ac.uk>
2017-02-28 14:29:44 +0000

    adding new library for investigating microclimate based on LiDAR-derived LAD profiles

---
+++ src/canopy_microclimate.py
@@ -0,0 +1,11 @@
+# This package hosts analyses to investigate sub-canopy microclimate based on the
+# vertical distribution of leaf area within the canopy.
+
+import numpy as np
+import sys
+from matplotlib import pyplot as plt
+import LiDAR_tools as lidar
+import auxilliary_functions as aux
+import LiDAR_MacHorn_LAD_profiles as LAD1
+import LiDAR_radiative_transfer_LAD_profiles as LAD2
+import inventory_based_LAD_profiles as field

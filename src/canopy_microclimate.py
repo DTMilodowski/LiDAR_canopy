719e298c240768dadff95e976fca1366dbf0ba4f
David Milodowski <d.t.milodowski@ed.ac.uk>
2017-02-28 14:55:28 +0000

    function to calculate fractional light absorption within canopy profile assuming Beer-Lambert-like decay

--- src/canopy_microclimate.py
+++ src/canopy_microclimate.py
@@ -17,10 +17,20 @@ import inventory_based_LAD_profiles as field
 #     I_v = I_0*exp(-k*sum(LAD[j=v:-1]))
 # k is the extinction coefficient defining Beer-Lambert-like decay of light
 # irradiance through the canopy
-# I_0 is the incident radiation at the top of the canopy 
+# I_0 is the incident radiation at the top of the canopy.  If I_0==1 this will
+# give the fraction of light transmitted to a given canopy layer.
 def estimate_canopy_light_transmittance(LAD,heights,k,I_0):
     N_levels = heights.size
     dz = np.abs(heights[1]-heights[0])
     I = I_0*np.exp( -k*np.cumsum(LAD[::-1])[::-1]*dz )
 
     return I
+
+# Estimate fraction of light absorbed at each layer within the canopy
+def estimate_canopy_light_absorption(I,k):
+    N_levels = I.size
+    temp_I = np.zeros(N_levels+1)
+    temp_I[1:] = I.copy
+    I0 = I[-1]
+    A = (temp_I[1:]-temp_I[:-1])/I0
+    return A

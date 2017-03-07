796df80dec2240c1efda724da6b7ad97307d505d
David Milodowski <d.t.milodowski@ed.ac.uk>
2017-03-07 11:17:23 +0000

    2D polynomial fit

--- src/least_squares_fitting.py
+++ src/least_squares_fitting.py
@@ -1,26 +1,27 @@
 import numpy as np
 
-def 1D_least_squares_polynomial(x,y,order):
+def oneD_least_squares_polynomial(x,y,order):
+    return 0
 
 
-
-def 2D_least_squares_polynomial(x,y,z,order):
+def twoD_least_squares_polynomial(x,y,z,order=1):
     # first up - mask nodata values
     mask = np.isfinite(z)
-    X = z[mask]
+    X = x[mask]
     Y = y[mask]
     Z = z[mask]
-    ones = np.zeros(Z.size)
+    ones = np.ones(Z.size)
     A=np.array([])
     
     if order == 1:
-        A = np.array([X,Y,ones])
+        # coefficients returned should be in order a-f for:
+        # z = ax + by + c
+        A = np.array([X,Y,ones]).T
     elif order == 2:
+        print "order = 2"
         # coefficients returned should be in order a-f for:
         # z = ax^2 + by^2 + cxy + dx + ey + f
-        A = np.array([X**2, Y**2, X*Y, X, Y, ones])
-
-    
+        A = np.array([X**2, Y**2, X*Y, X, Y, ones]).T
     b = Z.copy()
-
     coeff, r, rank, s = np.linalg.lstsq(A, b)
+    return coeff

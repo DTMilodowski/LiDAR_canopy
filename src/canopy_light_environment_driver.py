aae6e87cc061ebcb828dbcf1b2058e28598bbbb0
David Milodowski <d.t.milodowski@ed.ac.uk>
2017-04-10 13:24:11 +0100

    updates to driver

--- src/canopy_light_environment_driver.py
+++ src/canopy_light_environment_driver.py
@@ -15,20 +15,22 @@ import load_field_data as field
 # define the profiles' location
 LAD_file = './output/BALI_subplot_LAD_profiles_MacHorn_1m.npz'
 LAD_profiles = np.load(LAD_file)
+Plots = LAD_profiles.keys()
 heights = np.arange(1.,81.)
 light_absorption = {}
 light_transmittance = {}
-k = 0.034
-n_plots = len(LAD_profiles.keys())
+k = 0.7
+n_plots = len(Plots)
 color_string = 'blue'
 label_string = '-'
 for pp in range(0,n_plots):
-    plot = LAD_profiles.keys()[pp]
+    plot = Plots[pp]
     print plot
     I = np.zeros(LAD_profiles[plot].shape)
     A = np.zeros(LAD_profiles[plot].shape)
     n_sub = I.shape[0]
     for ss in range(0,n_sub):
+        print '======'
         I[ss,:]=clim.estimate_canopy_light_transmittance(LAD_profiles[plot][ss],heights,k)
         A[ss,:]=clim.estimate_canopy_light_absorption(I[ss,:],k)
 

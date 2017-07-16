6afe5ff0022eeb5d4dc3d335eddfe0bd3bade61b
David Milodowski <d.t.milodowski@ed.ac.uk>
2017-07-16 17:00:24 +0100

    adaptions to polygon filter code, now incorporating kdtree stuff

--- src/LiDAR_io.py
+++ src/LiDAR_io.py
@@ -8,7 +8,7 @@
 import numpy as np
 import laspy as las
 from scipy import spatial
-
+import LiDAR_tools as lidar
 
 # get bounding box from las file
 def get_lasfile_bbox(las_file):
@@ -43,9 +43,103 @@ def load_lidar_data(las_file,max_pts_per_tree = 10**6):
     for tt in range(0,ntrees):
         i0=tt*max_pts_per_tree
         i1 = (tt+1)*max_pts_per_tree
-        trees.append(spatial.cKDTree(pts[i0:i1,0:2],leafsize=32,balanced_tree=True))
+        if i1 < pts.size:
+            trees.append(spatial.cKDTree(pts[i0:i1,0:2],leafsize=32,balanced_tree=True))
+        else:
+            trees.append(spatial.cKDTree(pts[i0:,0:2],leafsize=32,balanced_tree=True))
         starting_ids[tt] = i0
 
     print "loaded ", pts[:,0].size, " points"
     return pts, starting_ids, trees
 
+
+#----------------------------------------------------------------------------
+## Now we have some more involved reading scripts that only load in the data
+## that satisfy certain neighbourhood criteria specified either as polygon or
+## a neighbourhood
+
+#----------------------------------------------------------------------------
+# First of all here are scripts that use a polygon to load subset
+#----------------------------------------------------------------------------
+
+# find all las files from a list that are located within a specified polygon
+def find_las_files_by_polygon(file_list,polygon):
+    las_files = np.genfromtxt(file_list,delimiter=',',dtype='S256')
+    keep = []
+    n_files = las_files.size
+    for i in range(0,n_files):
+        UR, LR, UL, LL = get_lasfile_bbox(las_files[i])
+        las_box = np.asarray([UR,LR,LL,UL])
+        x,y,inside = lidar.points_in_poly(polygon[:,0],polygon[:,1],las_box)
+        if inside.sum()>0:
+            keep.append(las_files[i])
+    print 'las tiles to load in:', len(keep)
+    for ll in range(0,len(keep)):
+        print keep[ll]
+    return keep
+
+# load all lidar points from multiple las files witin specified polygon.  The file list needs to have either the full or relative path to the files included.
+def load_lidar_data_by_polygon(file_list,polygon,max_pts_per_tree = 10**6):
+    keep_files = find_las_files_by_polygon(file_list,polygon)
+    n_files = len(keep_files)
+    trees = []
+    starting_ids = []
+
+    # first case scenario that no points in ROI
+    if n_files == 0:
+        print 'WARNING: No files within specified polygon - try again'
+        pts = np.array([])
+        
+    # otherwise, we have work to do!
+    else:
+        tile_pts = load_lidar_data(keep_files[0])
+        pts = lidar.filter_lidar_data_by_polygon(tile_pts,polygon)
+
+        # now create kdtrees
+        npts = pts.size
+        ntrees = np.ceil(npts/float(max_pts_per_tree));
+        trees = []
+        starting_ids = []
+        id = 0
+    
+        for tt in range(0,ntrees):
+            i0=tt*max_pts_per_tree
+            i1 = (tt+1)*max_pts_per_tree
+            if i1 < pts.size:
+                trees.append(spatial.cKDTree(pts[i0:i1,0:2],leafsize=32,balanced_tree=True))
+            else:
+                trees.append(spatial.cKDTree(pts[i0:,0:2],leafsize=32,balanced_tree=True))
+            starting_ids.append(id+i0)
+                
+        # now repeat for subsequent tiles
+        for i in range(1,n_files):
+            id = len(starting_ids)
+
+            tile_pts = load_lidar_data(keep_files[i])
+            pts_ = lidar.filter_lidar_data_by_polygon(tile_pts,polygon)
+
+            # create kdtrees
+            npts = pts_.size
+            ntrees = np.ceil(npts/float(max_pts_per_tree));
+            trees = []
+            starting_ids = []
+            id = 0
+    
+            for tt in range(0,ntrees):
+                i0=tt*max_pts_per_tree
+                i1 = (tt+1)*max_pts_per_tree
+                if i1 < pts_.size:
+                    trees.append(spatial.cKDTree(pts_[i0:i1,0:2],leafsize=32,balanced_tree=True))
+                else:
+                    trees.append(spatial.cKDTree(pts_[i0:,0:2],leafsize=32,balanced_tree=True))
+                starting_ids.append(id+i0)
+
+            # and finally concatenate the pts with previous ones
+            pts = np.concatenate((pts,pts_),axis=0)
+
+    print "loaded ", pts[:,0].size, " points"
+    return pts, np.asarray(starting_ids,dtype='int'), trees
+
+#----------------------------------------------------------------------------
+# Next here are scripts that use a point and circular neighbourhood instead
+#----------------------------------------------------------------------------

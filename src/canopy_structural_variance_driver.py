c2dd15bbccea5555ce4220696aa168d0fa281a2e
David Milodowski <d.t.milodowski@ed.ac.uk>
2017-07-18 12:20:07 +0100

    template for structure extraction ready, now need to decide on metrics

--- src/canopy_structural_variance_driver.py
+++ src/canopy_structural_variance_driver.py
@@ -6,7 +6,7 @@ import fiona
 import LiDAR_io as io
 import LiDAR_tools as lidar
 import auxilliary_functions as aux
-import LiDAR_MacHorn_LAD_profiles as LAD1
+import LiDAR_MacHorn_LAD_profiles as PAD
 
 from matplotlib import rcParams
 
@@ -24,6 +24,10 @@ pts_shp = 'sample_points.shp' ## CHANGE AS REQUIRED
 
 # Some parameters
 radius = 10.
+max_height = 80.
+heights = np.arange(0,max_height,layer_thickness)+layer_thickness
+kappa = 0.7
+layer_thickness = 1
 
 # Open shapefile and get points & bounding box for ROI
 shapefile= fiona.open(pts_shp)
@@ -40,3 +44,22 @@ N+=radius
 
 # Read in LiDAR points for region of interest
 # - first find the tiles
+polygon = np.asarray([[W,N],[E,N],[E,S],[W,S]])
+lidar_pts, starting_ids_for_trees, trees = io.load_lidar_data_by_polygon(lastile_list,polygon)
+N_trees = len(trees)
+
+
+sample_PAD = np.zeros((N_pts, heights.size))
+sample_pts = []
+# Now loop through the shapefile points and sample defined radius, before extracting canopy profiles
+for pp in range(0,N_pts):
+    for tt in range(0,N_trees):
+        # retrieve point clouds samples
+        ids = trees[tt].query_ball_point(pts[pp], radius)
+        sample_pts.append(lidar_pts[ids+starting_ids_for_trees[tt]])
+        
+    # calculate PAD profile
+    heights,first_return_profile,n_ground_returns = PAD.bin_returns(sample_pts[pp], max_height, layer_thickness)
+    PAD[pp,:] = PAD.estimate_LAD_MacArthurHorn(first_return_profile, n_ground_returns, layer_thickness, kappa)
+        
+# Compute metrics...

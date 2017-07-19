da99dd8ec0fc01d441b9031e829c5e9061166b63
David Milodowski <d.t.milodowski@ed.ac.uk>
2017-07-19 15:57:36 +0100

    applying affine transformation

--- src/structural_sensitivity_analysis.py
+++ src/structural_sensitivity_analysis.py
@@ -11,3 +11,19 @@ import LiDAR_radiative_transfer_LAD_profiles as LAD2
 import inventory_based_LAD_profiles as field
 import least_squares_fitting as lstsq
 from scipy import stats
+
+# First of all set up the sampling grids
+gps_pts_file = 'GPS_points_file_for_least_squares_fitting.csv'
+datatype = {'names': ('plot', 'x', 'y', 'x_prime', 'y_prime'), 'formats': ('S32','f16','f16','f16','f16')}
+plot_coordinates = np.genfromtxt(gps_pts_file, skiprows = 1, delimiter = ',',dtype=datatype)
+
+plot = 'Belian'
+alt_plot = 'maliau_belian'
+
+mask = plot_coordinates['plot']==alt_plot
+n_points_in_polygon = plot_coordinates['x'][mask].size
+
+affine=lstsq(plot_coordinates['x'][mask],plot_coordinates['y'][mask],plot_coordinates['x_prime'][mask],plot_coordinates['y_prime'][mask])
+x,y = lstsq.apply_affine_transformation(plot_coordinates['x'][mask],plot_coordinates['y'][mask])
+
+

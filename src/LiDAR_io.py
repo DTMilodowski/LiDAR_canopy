82ab848b8e2c0d8ff262c51cf89645d1df68b6c4
David Milodowski <d.t.milodowski@ed.ac.uk>
2017-07-28 17:10:57 +0100

    problem with auto-loading of las tiles

--- src/LiDAR_io.py
+++ src/LiDAR_io.py
@@ -103,10 +103,17 @@ def find_las_files_by_polygon(file_list,polygon):
 # polygon is a 2D array with N_pts*rows and two cols (x,y).
 # I've added a fudge to deal with laz files, which uses las2las from lastools to create a temporary .las file before reading with laspy. Undoubtedly not the most elegant solution, but it works :-) 
 def load_lidar_data_by_polygon(file_list,polygon,max_pts_per_tree = 10**6, laz_files=False):
+    W = polygon[:,0].min()
+    E = polygon[:,0].max()
+    S = polygon[:,1].min()
+    N = polygon[:,1].max()
+    print 'W: %.3f; E: %.3f; S: %.3f; N: %.3f' % (W,E,S,N)
+
     if laz_files:
         keep_files = find_laz_files_by_polygon(file_list,polygon)
     else:
         keep_files = find_las_files_by_polygon(file_list,polygon)
+    print keep_files
     n_files = len(keep_files)
     trees = []
     starting_ids = np.asarray([])
@@ -118,10 +125,6 @@ def load_lidar_data_by_polygon(file_list,polygon,max_pts_per_tree = 10**6, laz_f
                               
     # otherwise, we have work to do!
     else:
-        W = polygon[:,0].min()
-        E = polygon[:,0].max()
-        S = polygon[:,1].min()
-        N = polygon[:,1].max()
         if laz_files:
             os.system("las2las %s temp.las" % keep_files[i])
             tile_pts = load_lidar_data_by_bbox('temp.las',N,S,E,W,print_npts=False)
@@ -166,6 +169,7 @@ def find_laz_files_by_polygon(file_list,polygon):
     for i in range(0,n_files):
         os.system("las2las %s temp.las" % laz_files[i])
         UR, LR, UL, LL = get_lasfile_bbox('temp.las')
+        print 'W: %.3f; E: %.3f; S: %.3f; N: %.3f' % (LL[0],UR[0],LL[1],UR[1]) ## We have an issue with this test
         las_box = np.asarray([UR,LR,LL,UL])
         x,y,inside = lidar.points_in_poly(polygon[:,0],polygon[:,1],las_box)
         if inside.sum()>0:

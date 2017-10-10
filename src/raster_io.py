f06cebd6416060e800524c664cdd13c8ad96ba91
David Milodowski <d.t.milodowski@ed.ac.uk>
2017-10-10 12:59:30 +0100

    utm projection - need to check northup stuff, but at least seems to be working ok

--- src/raster_io.py
+++ src/raster_io.py
@@ -91,3 +91,104 @@ def write_raster_to_GeoTiff(array,geoTrans, OUTFILE_prefix, EPSG_CODE='4326', no
     dataset = None
     return 0
     #-----------------------------------
+
+
+# Function to write geotiff from an array
+def write_raster_to_GeoTiff_UTM(array,geoTrans, OUTFILE_prefix, utm_zone, northern_hemisphere = True, datum = "WGS84", north_up=True):
+
+    if northern_hemisphere == True:
+        hem = "northern"
+        bnorth = 1
+    else:
+        hem = "southern"
+        bnorth = 0
+        
+    prjCS = "UTM %i (%s) in %s hemisphere." % (utm_zone, datum, hem)
+    
+    #-----------------------------------
+    # some dummy variables to be updated
+    NBands = 1
+    NRows = 0
+    NCols = 0
+    #-----------------------------------
+
+    #-----------------------------------
+    # Check orientation of array before writing to raster to ensure 
+    # compatibility with GIS platforms.
+    if north_up:
+        # for north_up array, need the n-s resolution (element 5) to be negative
+        if geoTrans[5]>0:
+            geoTrans[5]*=-1
+            geoTrans[3] = geoTrans[3]-(array.shape[0]+1.)*geoTrans[5]
+        # Get array dimensions and flip so that it plots in the correct orientation on GIS platforms
+        if len(array.shape) < 2: 
+            print 'array has less than two dimensions! Unable to write to raster'
+            sys.exit(1)  
+        elif len(array.shape) == 2:
+            (NRows,NCols) = array.shape
+            #array = np.flipud(array)
+        elif len(array.shape) == 3:
+            (NRows,NCols,NBands) = array.shape
+            #for i in range(0,NBands):
+            #    array[:,:,i] = np.flipud(array[:,:,i])
+        else:
+            print 'array has too many dimensions! Unable to write to raster'
+            sys.exit(1)  
+
+    else:
+        # for north_up array, need the n-s resolution (element 5) to be positive
+        if geoTrans[5]<0:
+            geoTrans[5]*=-1
+            geoTrans[3] = geoTrans[3]-(array.shape[0]+1.)*geoTrans[5]
+        # Get array dimensions and flip so that it plots in the correct orientation on GIS platforms
+        if len(array.shape) < 2: 
+            print 'array has less than two dimensions! Unable to write to raster'
+            sys.exit(1)  
+        elif len(array.shape) == 2:
+            (NRows,NCols) = array.shape
+            array = np.flipud(array)
+        elif len(array.shape) == 3:
+            (NRows,NCols,NBands) = array.shape
+            for i in range(0,NBands):
+                array[:,:,i] = np.flipud(array[:,:,i])
+        else:
+            print 'array has too many dimensions! Unable to write to raster'
+            sys.exit(1)  
+    
+    # Get array dimensions and flip so that it plots in the correct orientation on GIS platforms
+    if len(array.shape) < 2: 
+        print 'array has less than two dimensions! Unable to write to raster'
+        sys.exit(1)  
+    elif len(array.shape) == 2:
+        (NRows,NCols) = array.shape
+        array = np.flipud(array)
+    elif len(array.shape) == 3:
+        (NRows,NCols,NBands) = array.shape
+        for i in range(0,NBands):
+            array[:,:,i] = np.flipud(array[:,:,i])
+    else:
+        print 'array has too many dimensions! Unable to write to raster'
+        sys.exit(1)  
+    #-----------------------------------
+
+    #-----------------------------------
+    # Write GeoTiff
+    driver = gdal.GetDriverByName('GTiff')
+    driver.Register()
+
+    # set all the relevant geospatial information
+    dataset = driver.Create( OUTFILE_prefix+'_data.tif', NCols, NRows, NBands, gdal.GDT_Float32 )
+    dataset.SetGeoTransform( geoTrans )
+    srs = osr.SpatialReference()
+    srs.SetProjCS( prjCS )
+    srs.SetWellKnownGeogCS( datum )
+    srs.SetUTM( utm_zone, bnorth )
+    #srs.SetWellKnownGeogCS( 'EPSG:'+EPSG_CODE )
+    dataset.SetProjection( srs.ExportToWkt() )
+    # write array
+    dataset.GetRasterBand(1).SetNoDataValue( -9999 )
+    dataset.GetRasterBand(1).WriteArray( array )
+    dataset = None
+    return 0
+    #-----------------------------------
+

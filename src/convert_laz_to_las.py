f8e2f98ce6b6c108fab257f85e920111ecb891cc
David Milodowski <d.t.milodowski@ed.ac.uk>
2017-10-13 13:55:46 +0100

    ready to go

--- src/convert_laz_to_las.py
+++ src/convert_laz_to_las.py
@@ -10,4 +10,14 @@ laz_dir = '/disk/scratch/local.2/dmilodow/BALI/LiDAR/Danum/Danum_CHM_laz_tiles'
 las_dir = '/disk/scratch/local.2/dmilodow/BALI/LiDAR/Danum'
 
 # create file list
-os.system("ls %s/*.laz > %s/laz_list.txt" % (laz_dir,lazdir))
+os.system("ls %s/*.laz > %s/laz_list.txt" % (laz_dir,laz_dir))
+laz_files = np.genfromtxt("%s/laz_list.txt" % laz_dir,delimiter=',',dtype='S256')
+N = len(laz_files)
+for ii in range(0,N):
+    filename = laz_files[ii].split('/')[-1]
+    prefix = filename.split('.')[0]
+    print "tile %i of %i: %s" % (ii+1,N,prefix)
+    las_file = '%s/%s.las' % (las_dir,prefix)
+    os.system("las2las %s %s" % (laz_file, las_file))
+    
+print "Done" 

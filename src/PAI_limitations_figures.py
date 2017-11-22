5cdfbfa8e9ec84799a412d2f6247676babf8d5cf
David Milodowski <d.t.milodowski@ed.ac.uk>
2017-11-22 12:28:07 +0000

    plot params

--- src/PAI_limitations_figures.py
+++ src/PAI_limitations_figures.py
@@ -12,19 +12,41 @@ import numpy as np
 import matplotlib.pyplot as plt
 from matplotlib.patches import Polygon
 from mpl_toolkits.basemap import Basemap, shiftgrid
+from matplotlib import rcParams
 import sys
 sys.path.append('/home/dmilodow/DataStore_DTM/FOREST2020/EOdata/EO_data_processing/src/plot_EO_data/colormap/')
-
 import colormaps as cmaps
 plt.register_cmap(name='viridis', cmap=cmaps.viridis)
 plt.register_cmap(name='plasma', cmap=cmaps.plasma)
 plt.set_cmap(cmaps.viridis)
 
+# Set up some basiic parameters for the plots
+rcParams['font.family'] = 'sans-serif'
+rcParams['font.sans-serif'] = ['arial']
+rcParams['font.size'] = 8
+rcParams['legend.numpoints'] = 1
+axis_size = rcParams['font.size']+2
+colour = ['#46E900','#1A2BCE','#E0007F']
+
+
 # import required LiDAR libaries
 import LiDAR_MacHorn_LAD_profiles.py as MH
+import raster_io as io
+
+# Load files
+dens_file = 'SAFE_pointcloud_metrics_10m_point_density_data.tif'
+PAI_file = 'SAFE_pointcloud_metrics_10m_pai_data.tif'
+
+dens, geo, coord = io.load_GeoTIFF_band_and_georeferencing(dens_file)
+PAI, geo, coord = io.load_GeoTIFF_band_and_georeferencing(PAI_file)
 
 #-------------------------------------------------------------------------------
 # Figure 1 - this figure illustrates the analytical solution presented in this
 # paper describing the threshold PAI that can be detected using discrete return
 # LiDAR
+
+
+#-------------------------------------------------------------------------------
+# Figure 2 - this figure presents maps of PAI and point density across the SAFE
+# landscape
 

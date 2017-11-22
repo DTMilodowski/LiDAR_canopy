59886a8cdb16b76e4881026afc55a75b3a7172c9
David Milodowski <d.t.milodowski@ed.ac.uk>
2017-11-22 12:17:36 +0000

    new func for paper figs

---
+++ src/PAI_limitations_figures.py
@@ -0,0 +1,7 @@
+#===============================================================================
+# PAI_limitations_figures.py
+# D.T.Milodowski, November 2017
+#-------------------------------------------------------------------------------
+# This function contains the scripts used to produce the figures in the paper:
+# "Point density imposes limitations on PAI estimation"
+#-------------------------------------------------------------------------------

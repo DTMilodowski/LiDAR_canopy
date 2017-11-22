3ca1bcf63db12d143cb94683431f1c58049762ae
David Milodowski <d.t.milodowski@ed.ac.uk>
2017-11-22 14:27:03 +0000

    plotting analytical vs reality

--- src/PAI_limitations_figures.py
+++ src/PAI_limitations_figures.py
@@ -52,12 +52,12 @@ theta_deg = np.asarray([0, 5, 10])
 theta_rad = theta_deg*np.pi/180.
 radius = np.asarray([10.,20.,30.])
 area = np.pi*radius**2
-dens_a = np.arange(0.01,30,0.01)
+dens_a = np.arange(0.01,np.nanmax(dens),0.01)
 
 PAImax_10m_00deg = MH.calculate_analytical_limit(dens_a,area[0],k,theta_rad[0])
 PAImax_20m_00deg = MH.calculate_analytical_limit(dens_a,area[1],k,theta_rad[0])
 PAImax_30m_00deg = MH.calculate_analytical_limit(dens_a,area[2],k,theta_rad[0])
-
+"""
 PAImax_10m_05deg = MH.calculate_analytical_limit(dens_a,area[0],k,theta_rad[1])
 PAImax_20m_05deg = MH.calculate_analytical_limit(dens_a,area[1],k,theta_rad[1])
 PAImax_30m_05deg = MH.calculate_analytical_limit(dens_a,area[2],k,theta_rad[1])
@@ -65,13 +65,38 @@ PAImax_30m_05deg = MH.calculate_analytical_limit(dens_a,area[2],k,theta_rad[1])
 PAImax_10m_10deg = MH.calculate_analytical_limit(dens_a,area[0],k,theta_rad[2])
 PAImax_20m_10deg = MH.calculate_analytical_limit(dens_a,area[1],k,theta_rad[2])
 PAImax_30m_10deg = MH.calculate_analytical_limit(dens_a,area[2],k,theta_rad[2])
-
+"""
 #-------------------------------------------------------------------------------
 # Figure 1 - this figure illustrates the analytical solution presented in this
 # paper describing the threshold PAI that can be detected using discrete return
 # LiDAR
+fig = plt.figure(1, facecolor='White',figsize=[6,3])
+ax1a= plt.subplot2grid((1,2),(0,0))
+ax1a.annotate('a', xy=(0.05,0.95), xycoords='axes fraction',backgroundcolor='none',horizontalalignment='left', verticalalignment='top', fontsize=10)
+ax1a.set_xlabel('point density / pts m$^{-2}$',fontsize = axis_size)
+ax1a.set_ylabel('PAI$_{max}$',fontsize = axis_size)
 
+ax1a.plot(dens_a,PAImax_10m_00deg,'-',c=colour[0],label = '%.2f ha' % (np.pi*radius[0]**2/10.**4))
+ax1a.plot(dens_a,PAImax_20m_00deg,'-',c=colour[1],label = '%.2f ha' % (np.pi*radius[1]**2/10.**4))
+ax1a.plot(dens_a,PAImax_30m_00deg,'-',c=colour[2],label = '%.2f ha' % (np.pi*radius[2]**2/10.**4))
+ax1a.legend(loc='lower right')
 
+ax1b= plt.subplot2grid((1,2),(0,1),sharex=ax1a,sharey=ax1a)
+ax1b.annotate('b', xy=(0.05,0.95), xycoords='axes fraction',backgroundcolor='none',horizontalalignment='left', verticalalignment='top', fontsize=10, color='white')
+ax1b.set_xlabel('point density / pts m$^{-2}$',fontsize = axis_size)
+ax1b.set_ylabel('PAI',fontsize = axis_size)
+
+hb = ax1b.hexbin(dens.reshape(dens.size), PAI.reshape(PAI.size), gridsize=50, bins='log', cmap='plasma')
+cb = fig.colorbar(hb, ax=ax1b)
+cb.set_label('log$_{10}$(Number of grid cells)')
+
+ax1b.plot(dens_a,PAImax_10m_00deg,'-',c='white')
+ax1b.set_xlim(0,100)
+ax1b.set_ylim(0,np.nanmax(PAI))
+plt.tight_layout()
+plt.show()
+
+plt.savefig(SAVEDIR+'Fig1_SAFE_point_density_vs_PAI.png')
 
 #-------------------------------------------------------------------------------
 # Figure 2 - this figure presents maps of PAI and point density across the SAFE

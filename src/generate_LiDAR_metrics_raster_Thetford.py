2aecdf3a24aa3dfb3884aa50fecf9aeefa37a594
David Milodowski <d.t.milodowski@ed.ac.uk>
2018-07-31 17:11:18 +0100

    Thetford script ready to go pending new tiles

--- src/generate_LiDAR_metrics_raster_Thetford.py
+++ src/generate_LiDAR_metrics_raster_Thetford.py
@@ -25,47 +25,60 @@ import laspy as las
 import LiDAR_io as io
 import LiDAR_tools as lidar
 import auxilliary_functions as aux
-import LiDAR_MacHorn_LAD_profiles as PAD
+import LiDAR_MacHorn_LAD_profiles as pad
 import structural_metrics as struct
 import raster_io as raster
 
 #-------------------------------------------------------------------------------
 # Input files
 laz_list = 'thetford2015_lazfiles.txt'
-laz_files = True ## CHANGE AS REQUIRED
+laz_files = False ## CHANGE AS REQUIRED - note that if laszip is installed, this
+                  ## can safely be set to False for both las and laz files. If
+                  ## True, this leads to a workaround for cases where laszip is
+                  ## not available, in which las2las is called to write a temp
+                  ## las file. This process is slow, so the recommendation is to
+                  ## install laszip!
 
 # Site ID
-site = 'Thetford2015'
+site = 'Thetford2015_patchN'
 
 
 # Some parameters
 min_PAD = 0.1
 radius = np.sqrt(200)
-max_height = 25.   
+max_height = 46.   
 min_height = 2.     
 layer_thickness = 1
 heights = np.arange(0,max_height,layer_thickness)+layer_thickness
 layers = heights.size
 kappa = 1.
-raster_res = 5
-
-# Some georeferencing info
-utm = 31
+raster_res = 10
 
 #-------------------------------------------------------------------------------
 # Phase one - get bounding box of all las tiles.
-UR, LR, UL, LL = io.get_bbox_of_multiple_tiles(laz_list,laz_files)
+# - define bbox for region 1 (N)
+N_ = 301000.
+S_ = 280000.
+E_ = 587000.
+W_ = 573500.
+"""
+# define bbox for region 2 (E)
+N_ = 295500
+S_ = 280500
+E_ = 600500
+W_ = 590500
 
-# Phase two - construct host arrays
-xmin = LL[0]
-xmax = LR[0]
-ymin = LL[1]
-ymax = UL[1]
+# define bbox for region 3 (S)
+N_ = 278000
+S_ = 267000
+E_ = 586000
+W_ = 578000
+"""
 
-LL=None; LR=None; UL=None; UR=None
+bbox  = np.array([[W_,N_],[E_,N_],[E_,S_],[W_,S_]])
 
-x_coords = np.arange(xmin+raster_res/2.,xmax,raster_res)
-y_coords = np.arange(ymin+raster_res/2.,ymax,raster_res)
+x_coords = np.arange(W_+raster_res/2.,E_+raster_res,raster_res)
+y_coords = np.arange(S_+raster_res/2.,N_+raster_res,raster_res)
 
 rows = y_coords.size
 cols = x_coords.size
@@ -73,30 +86,25 @@ rows_ii = np.arange(y_coords.size)
 cols_jj = np.arange(x_coords.size)
 
 PAI = np.zeros((rows,cols))*np.nan
-PAD = np.zeros((layers,rows,cols))*np.nan
+PAD = np.zeros((rows,cols,layers))*np.nan
 Shannon = np.zeros((rows,cols))*np.nan
 pt_dens = np.zeros((rows,cols))
 
-
 # Phase three - loop through las tiles and gradually fill the array
-laz_files = np.genfromtxt(laz_list,delimiter=',',dtype='S256')
-n_files = laz_files.size
+laz_files = io.find_las_files_by_polygon(laz_list,bbox)
+n_files = len(laz_files)
 for i in range(0,n_files):
     print "Processing tile %i of %i" % (i+1,n_files)
     # get bbox of specific tile
-    if laz_files:
-        os.system("las2las %s temp.las" % laz_files[i])
-        lasFile = las.file.File('temp.las',mode='r')
-        max_xyz = lasFile.header.max
-        min_xyz = lasFile.header.min
-        lasFile.close()
-        os.system("rm temp.las")
-    else:
-        lasFile = las.file.File(laz_files[i],mode='r')
-        max_xyz = lasFile.header.max
-        min_xyz = lasFile.header.min
-        lasFile.close()
-
+    lasFile = las.file.File(laz_files[i],mode='r-')
+    max_xyz = lasFile.header.max
+    min_xyz = lasFile.header.min
+    lasFile.close()
+    """
+    plt.plot([max_xyz[0],min_xyz[0],min_xyz[0],max_xyz[0],max_xyz[0]],[max_xyz[1],max_xyz[1],min_xyz[1],min_xyz[1],max_xyz[1]],'-')
+    plt.plot([W_,W_,E_,E_,W_],[N_,S_,S_,N_,N_],'--')
+    plt.axis('equal');plt.show()
+    """
     # buffer this bounding box with the search radius
     E = max_xyz[0]+radius
     N = max_xyz[1]+radius
@@ -105,7 +113,7 @@ for i in range(0,n_files):
 
     # Read in LiDAR points for region of interest
     polygon = np.asarray([[W,N],[E,N],[E,S],[W,S]])
-    lidar_pts, starting_ids_for_trees, trees = io.load_lidar_data_by_polygon(las_list,polygon,max_pts_per_tree = 5*10**5, laz_files=laz_files)
+    lidar_pts, starting_ids_for_trees, trees = io.load_lidar_data_by_polygon(laz_list,polygon,max_pts_per_tree = 5*10**5)
     N_trees = len(trees)
 
     # Get the positions and indices of grid points within the bounds of the tile
@@ -146,27 +154,27 @@ for i in range(0,n_files):
             if sample_pts.size > 0:
                 if np.sum(sample_pts[:,3]==1) > 0:
                     # calculate PAD profile
-                    heights,first_return_profile,n_ground_returns = PAD.bin_returns(sample_pts, max_height, layer_thickness)
-                    PADprof = PAD.estimate_LAD_MacArthurHorn(first_return_profile, n_ground_returns, layer_thickness, kappa)
+                    heights,first_return_profile,n_ground_returns = pad.bin_returns(sample_pts, max_height, layer_thickness)
+                    PADprof = pad.estimate_LAD_MacArthurHorn(first_return_profile, n_ground_returns, layer_thickness, kappa,zero_nodata=True)
+                    
+                    # vertically distributed PAI
+                    PAD[:,row_ii,col_jj] = PADprof.copy()
                     
                     # remove lowermost portion of profile
                     PAD_iter = PADprof.copy()
                     PAD_iter[heights<min_height]=0
-                
                     PAI[row_ii,col_jj] = np.nansum(PAD_iter)
 
-                    # vertically distributed PAI
-                    PAD[:,row_ii,col_jj] = PAD_prof.copy()
 
                     # other metrics
                     pt_dens[row_ii,col_jj] = sample_pts.shape[0]/(np.pi*radius**2.)
-                    Shannon[row_ii,col_jj] = struct.calculate_Shannon_index(PAD_iter[np.isnan(PAD_iter))
+                    Shannon[row_ii,col_jj] = struct.calculate_Shannon_index(PAD_iter[np.isnan(PAD_iter)])
             sample_pts=None
     lidar_pts = None
     trees = None
     starting_ids_for_trees = None
 
-np.savez('%s_metrics_10m' % site,point_density=pt_dens,pai=PAI,shannon=Shannon,shape=Shape,pai_02_10m=PAI10,pai_10_20m=PAI20,pai_20_30m=PAI30,pai_30_40m=PAI40,pai_40_50m=PAI50,pai_50_60m=PAI60,pai_60_70m=PAI70,pai_70_80m=PAI80,n_layers=layers, canopy_height = can_ht, mean=mean, std=std, skew=skew,kurt=kurt)
+np.savez('%s_metrics_10m' % site,point_density=pt_dens,pai=PAI,shannon=Shannon, pad=PAD)
 
 metrics = np.load('%s_metrics_10m.npz' % site)
 
@@ -178,7 +186,7 @@ geoTransform = [ XMinimum, raster_res, 0, YMaximum, 0, -raster_res ]
 for kk in range(0,len(metrics.keys())):
     var = metrics.keys()[kk]
     print "\t\t\t Saving rasters: %s" % var
-    raster.write_raster_to_GeoTiff_UTM(metrics[var], geoTransform, ('%s_pointcloud_metrics_20m_%s' % (site,var)), utm)
+    raster.write_raster_to_GeoTiff(metrics[var], geoTransform, ('%s_pointcloud_metrics_10m_%s' % (site,var)), EPSG_CODE='27700')
 
 """
 import sys

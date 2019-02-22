a4638fb679e15c98c7c3138f283e268a9046d6a9
David Milodowski <d.t.milodowski@ed.ac.uk>
2019-02-22 16:09:42 +0000

    finished tweaking the violins

--- src/sensitivity_analysis_figures_revised.py
+++ src/sensitivity_analysis_figures_revised.py
@@ -1,5 +1,9 @@
 import numpy as np
 import sensitivity_analysis_plots as sp
+
+output_dir = '/home/dmilodow/DataStore_DTM/BALI/PAPERS/PaperDrafts/EstimatingCanopyStructureBALI/FiguresRevised/'
+
+
 # Load in the sensitivity analysis results
 max_height=80
 heights = np.arange(0.,max_height)+1
@@ -25,7 +29,7 @@ penetration_lim_E = np.load('penetration_limit_E.npy')[()]
  Comparison of OG vs Moderately Logged vs. Heavily Logged
 """
 figure_number = 8
-figure_name = "fig8_profile_sensitivity_to_resolution.png"
+figure_name = output_dir + "fig8_profile_sensitivity_to_resolution.png"
 sp.plot_profile_sensitivity_resolution(figure_number,figure_name,heights,PAD_profiles_MH_Belian,
                             PAD_profiles_MH_BNorth,PAD_profiles_MH_E,PAD_profiles_rad_Belian,
                             PAD_profiles_rad_BNorth,PAD_profiles_rad_E)
@@ -34,7 +38,7 @@ sp.plot_profile_sensitivity_resolution(figure_number,figure_name,heights,PAD_pro
 Figure 9 - Sensitivity analysis of unsampled voxels
 """
 figure_number = 9
-figure_name = "figs9_unsampled_voxels.png"
+figure_name = output_dir + "figs9_unsampled_voxels.png"
 sp.plot_penetration_limits(figure_number,figure_name,heights,penetration_lim_Belian,
                             penetration_lim_E,penetration_lim_BNorth)
 
@@ -42,7 +46,7 @@ sp.plot_penetration_limits(figure_number,figure_name,heights,penetration_lim_Bel
 # Figure 10 - Sensitivity analysis of vertical profiles to point density
 """
 figure_number = 10
-figure_name = "fig10_profile_sensitivity_to_point_density.png"
+figure_name = output_dir + "fig10_profile_sensitivity_to_point_density.png"
 sp.plot_profile_sensitivity_point_density(figure_number,figure_name,heights,PAD_profiles_MH_Belian,
                             PAD_profiles_MH_BNorth,PAD_profiles_MH_E,PAD_profiles_rad_Belian,
                             PAD_profiles_rad_BNorth,PAD_profiles_rad_E)
@@ -52,8 +56,10 @@ sp.plot_profile_sensitivity_point_density(figure_number,figure_name,heights,PAD_
 # and point density
 """
 figure_number = 11
-figure_name = "fig11_PAI_sensitivity.png"
-
+figure_name = output_dir + "fig11_PAI_sensitivity.png"
+sp.plot_PAI_sensitivity(figure_number,figure_name,PAD_profiles_MH_Belian,PAD_profiles_MH_BNorth,
+                        PAD_profiles_MH_E,PAD_profiles_rad2_Belian,
+                        PAD_profiles_rad2_BNorth,PAD_profiles_rad2_E)
 
 #-------------------------------
 # SUPPLEMENT
@@ -63,13 +69,13 @@ figure_name = "fig11_PAI_sensitivity.png"
 # Figure S5 - sensitivity analysis, confidence interval sensitivity to resolution
 """
 figure_number = 115
-figure_name = "figS5_profile_sensitivity_to_resolution_individual_CI.png"
+figure_name = output_dir + "figS5_profile_sensitivity_to_resolution_individual_CI.png"
 sp.plot_profile_sensitivity_to_resolution_individual_CI(figure_number,figure_name,heights,
                     PAD_profiles_MH_Belian,PAD_profiles_rad_Belian)
 """
 # Figure S6 - sensitivity analysis, confidence interval sensitivity to density
 """
 figure_number = 116
-figure_name = "figS6_profile_sensitivity_to_point_density_individual_CI.png"
+figure_name = output_dir + "figS6_profile_sensitivity_to_point_density_individual_CI.png"
 sp.plot_profile_sensitivity_to_point_density_individual_CI(figure_number,figure_name,heights,
                     PAD_profiles_MH_Belian,PAD_profiles_rad_Belian)

157f54e728b97cbcf34f0aa0773e6e2fb5c6c141
David Milodowski <d.t.milodowski@ed.ac.uk>
2019-07-17 10:52:51 +0100

    adding oil palm

--- src/BALI_synthesis_contribution/synthesis_figure.py
+++ src/BALI_synthesis_contribution/synthesis_figure.py
@@ -13,9 +13,9 @@ import seaborn as sns
 sns.set()
 #---------------------------------------------------------------------------------------------------------------
 # Some filenames & params
-plots_b = [b'Belian',b'E',b'B North']
-plots = ['Belian','E','B North']
-chm_files = ['CHM_50cm_Belian.npy','CHM_50cm_E.npy','CHM_50cm_BNorth.npy']
+plots_b = [b'Belian',b'E',b'B North',b'OP']
+plots = ['Belian','E','B North','OP']
+chm_files = ['CHM_50cm_Belian.npy','CHM_50cm_E.npy','CHM_50cm_BNorth.npy','CHM_50cm_OP.npy']
 chm = {}
 x = np.arange(0,100,0.5)+.25
 y = np.arange(0,100,0.5)+.25
@@ -25,6 +25,8 @@ for pp,plot in enumerate(plots):
 
 profile_file = 'lidar_canopy_profiles_adaptive.npz'
 canopy_profiles = np.load(profile_file)['arr_0'][()]
+op_file = 'lidar_canopy_profiles_adaptive_OP.npz'
+canopy_profiles[b'OP']=np.load(op_file)['arr_0'][()]['OP']
 
 gps_pts_file = '../GPS_points_file_for_least_squares_fitting_sensitivity_analysis_version.csv'
 datatype = {'names': ('plot', 'x', 'y', 'x_prime', 'y_prime'), 'formats': ('<U16','f16','f16','f16','f16')}
@@ -38,13 +40,17 @@ plot_width = 100.
 n_arrow={}
 n_arrow_text={}
 for plot in (plots):
-    mask = plot_coordinates['plot']==plot
-    affine=lstsq.least_squares_affine_matrix(plot_coordinates['x_prime'][mask],plot_coordinates['y_prime'][mask],
-                                            plot_coordinates['x'][mask],plot_coordinates['y'][mask])
-    n_arrow[plot] = np.array(lstsq.apply_affine_transformation(np.array([0.,0.]),
-                                        np.array([0.,15.]),affine)).transpose() # simple square bounding box applied for all sensitivity analyses
-    n_arrow_text[plot] = np.array(lstsq.apply_affine_transformation(np.array([0.,0.]),
-                                        np.array([0.,18.]),affine)).transpose() # simple square bounding box applied for all sensitivity analyses
+    if plot in plot_coordinates['plot']:
+        mask = plot_coordinates['plot']==plot
+        affine=lstsq.least_squares_affine_matrix(plot_coordinates['x_prime'][mask],plot_coordinates['y_prime'][mask],
+                                                plot_coordinates['x'][mask],plot_coordinates['y'][mask])
+        n_arrow[plot] = np.array(lstsq.apply_affine_transformation(np.array([0.,0.]),
+                                            np.array([0.,15.]),affine)).transpose() # simple square bounding box applied for all sensitivity analyses
+        n_arrow_text[plot] = np.array(lstsq.apply_affine_transformation(np.array([0.,0.]),
+                                            np.array([0.,18.]),affine)).transpose() # simple square bounding box applied for all sensitivity analyses
+    else:
+        n_arrow[plot] = np.array(([0.,0.],[0.,15.])) # simple square bounding box applied for all sensitivity analyses
+        n_arrow_text[plot] = np.array(([0.,0.],[0.,18]))
 
     shift_x = np.max(n_arrow[plot][:,0])-90
     shift_y = np.max(n_arrow[plot][:,1])-90
@@ -54,10 +60,10 @@ for plot in (plots):
     n_arrow_text[plot][:,1]-=shift_y
 
 # Plot up the figure
-labels = ['Old growth (MLA-01)','Moderately logged (SAF-03)','Heavily logged (SAF-02)']
-fig,axes = plt.subplots(nrows=1,ncols=3,figsize=[9,3.5],subplot_kw={'aspect':'equal',
+labels = ['Old growth (MLA-01)','Moderately logged (SAF-03)','Heavily logged (SAF-02)','Oil Palm (OP)']
+fig,axes = plt.subplots(nrows=2,ncols=2,figsize=[6,6],subplot_kw={'aspect':'equal',
                         'xlim':(0,100),'ylim':(0,100)})
-for pp,ax in enumerate(axes):
+for pp,ax in enumerate(axes.ravel()):
     mean_prof = np.mean(canopy_profiles[plots_b[pp]],axis=0)
     sem_prof = stats.sem(canopy_profiles[plots_b[pp]],axis=0)
     # chm
@@ -68,14 +74,8 @@ for pp,ax in enumerate(axes):
     # N arrow
     N = n_arrow[plots[pp]]
     N_txt = n_arrow_text[plots[pp]]
-    #line = ax.plot(N[:,0],N[:,1],'-',color='white')[0]
     ax.annotate('',xytext=N[0],xy=N[1],arrowprops=dict(arrowstyle='-|>',color='white'))
     ax.annotate('N',xy=N_txt[1],color='white',va='center',ha='center',fontsize=10)
-    #ax.axis('equal')
-    #ax.set_ylim(bottom=0,top=100)
-    #ax.set_xlim(left=0,right=100)
-    ax.set_xticklabels(ax.get_xticklabels(),visible=False)
-    ax.set_yticklabels(ax.get_yticklabels(),visible=False)
     # scale bar
     ax.annotate('20m',xy=(92.5,15),color='white',va='center',ha='right',fontsize=10)
     ax.plot((95,95),(5,25),'-',color='white',lw=1)
@@ -83,6 +83,9 @@ for pp,ax in enumerate(axes):
     ax.plot((92.5,97.5),(5,5),'-',color='white',lw=1)
     # axis label
     ax.set_title(labels[pp])
+    ax.set_xticklabels(ax.get_xticklabels(),visible=False)
+    ax.set_yticklabels(ax.get_yticklabels(),visible=False)
+
 fig.subplots_adjust(right=0.8)
 cbar_ax = fig.add_axes([0.84,0.245,0.04,0.51])
 fig.colorbar(im,cax=cbar_ax, label='canopy height / m')

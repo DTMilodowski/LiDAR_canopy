dd028c0a3e4b1e1b22bfeed9dfbf6206af77cbc3
David Milodowski <d.t.milodowski@ed.ac.uk>
2019-09-04 15:18:29 +0100

    loading data

--- src/scottish_understory/SEOS_lidar_summary_figures.py
+++ src/scottish_understory/SEOS_lidar_summary_figures.py
@@ -9,8 +9,8 @@ Encompasses:
 - Canopy density profiles, inverted from point clouds
     - a random selection of 5m x 5m resolution profiles
 - Sensitivity analysis to spatial resolution (1 ha)
-- Sensitivity analysis to point density (1 ha)
-- Sensitivity analysis to point density (5m x 5m individual profiles)
+- Sensitivity analysis to pulse density (1 ha)
+- Sensitivity analysis to pulse density (5m x 5m individual profiles)
 --------------------------------------------------------------------------------
 """
 # import standard libraries
@@ -37,8 +37,14 @@ LiDAR data
 Sensitivity analyses results
 --------------------------------------------------------------------------------
 """
-# Load LiDAR data by bounding box
+# Define paths etc
+file_id = 'carbomap_site1_5m'
+path2data = '/exports/csce/datastore/geos/users/dmilodow/FOREST2020/LiDAR/carbomap_highlands/canopy_metrics/'
+path2fig = '/exports/csce/datastore/geos/users/dmilodow/FOREST2020/LiDAR/carbomap_highlands/figures/'
 las_list = '../las_lists/carbomap_site1_lastiles.txt'
+pad_file = '%s%s_pad.tif' % (path2data,file_id)
+
+# Load LiDAR data by bounding box
 N=6206000.
 S=6205900.
 W=344520.
@@ -50,15 +56,17 @@ N_trees = len(trees)
 pts[np.any((pts[:,4]==3,pts[:,4]==4,pts[:,4]==5),axis=0),4]=1
 pts[pts[:,2]<0,2]=0
 
+# Load sensitivity analysis to spatial resolution
+resolution_sensitivity = np.load('SEOS_MH_sensitivity_resolution.npy')[()]
+density_sensitivity = np.load('SEOS_MH_sensitivity_pulse_density.npy')[()]
 
+# Load 3D maps of canopy density
+pad = xr.open_rasterio(pad_file)
 
-
-
-
+# other parameters
 max_height = 40.
 layer_thickness = 0.5
 heights = np.arange(0.,max_height,layer_thickness)+0.5
-heights_rad = np.arange(0,max_height+0.5,layer_thickness)
 n_layers = heights.size
 plot_width = 100.
 sample_res = 5.

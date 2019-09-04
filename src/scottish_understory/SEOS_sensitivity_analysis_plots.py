116d7a15e28a15ec6647f5918aeb7269dac62c3d
David Milodowski <d.t.milodowski@ed.ac.uk>
2019-09-04 15:39:31 +0100

    refining density sensitivity plot

--- src/scottish_understory/SEOS_sensitivity_analysis_plots.py
+++ src/scottish_understory/SEOS_sensitivity_analysis_plots.py
@@ -10,13 +10,7 @@ from mpl_toolkits.axes_grid1.inset_locator import inset_axes
 
 import seaborn as sns
 import pandas as pd
-
-rcParams['font.family'] = 'sans-serif'
-rcParams['font.sans-serif'] = ['arial']
-rcParams['font.size'] = 8
-rcParams['legend.numpoints'] = 1
-axis_size = rcParams['font.size']+2
-colour = ['#46E900','#1A2BCE','#E0007F']
+sns.set()
 
 """
 Plot sensitivity of 1ha profiles to spatial resolution
@@ -71,23 +65,15 @@ def plot_profile_sensitivity_resolution(figure_number,figure_name,heights,PAD_pr
 
 def plot_profile_sensitivity_density(figure_number,figure_name,heights,PAD_profiles_MH):
 
-    ## Belian
-    fig = plt.figure(figure_number, facecolor='White',figsize=[8,4])
-    axa = plt.subplot2grid((1,5),(0,0))
-    axb = plt.subplot2grid((1,5),(0,1),sharex=axa,sharey=axa)
-    axc = plt.subplot2grid((1,5),(0,2),sharex=axa,sharey=axa)
-    axd = plt.subplot2grid((1,5),(0,3),sharex=axa,sharey=axa)
-    axe = plt.subplot2grid((1,5),(0,4),sharex=axa,sharey=axa)
-
-    annotations = ['a - 20 shots m$^{-2}$','b - 50 shots m$^{-2}$',
-                    'c - 100 shots m$^{-2}$','d - 500 shots m$^{-2}$',
-                    'e - 1000 shots m$^{-2}$']
-    axes = [axa,axb,axc,axd,axe]
+    fig,axes = plt.subplots(1,5,sharey="all",sharex="all",figsize=[8,3])
+    annotations = ['20 shots m$^{-2}$','50 shots m$^{-2}$',
+                    '100 shots m$^{-2}$','500 shots m$^{-2}$',
+                    '1000 shots m$^{-2}$']
+    #axes = [axa,axb,axc,axd,axe]
     # loop through the subplots
     keys = ['20','50','100','500','1000']
     for ii,ax in enumerate(axes):
-        ax.annotate(annotations[ii], xy=(0.05,0.95), xycoords='axes fraction',
-                    backgroundcolor='none',ha='left', va='top', fontsize=8)
+        ax.set_title(annotations[ii])
         #rad_sp = np.nanmean(PAD_profiles_rad[keys[ii]],axis=1)
         MH_sp = np.nanmean(PAD_profiles_MH[keys[ii]],axis=1)
         MH = np.nanmedian(MH_sp,axis=0)
@@ -100,20 +86,19 @@ def plot_profile_sensitivity_density(figure_number,figure_name,heights,PAD_profi
         else:
             ax.plot(MH[2:],heights[2:],'-',c=colour[0],linewidth=1.5)
         print('%.3f' % np.sum(MH[2:]))
-    yticklabels = axb.get_yticklabels() + axc.get_yticklabels() + axd.get_yticklabels() + axe.get_yticklabels()
-    plt.setp(yticklabels,visible=False)
+    #yticklabels = axb.get_yticklabels() + axc.get_yticklabels() + axd.get_yticklabels() + axe.get_yticklabels()
+    #plt.setp(yticklabels,visible=False)
 
-    axa.set_ylim(0,40)
-    axa.set_xlim(0,0.55)
+    axes[0].set_ylim(0,40)
+    axes[0].set_xlim(0,0.35)
     for ax in axes:
         ax.locator_params(axis='x',nbins=5)
-        ax.yaxis.set_ticks_position('both')
+        #ax.yaxis.set_ticks_position('both')
 
-    plt.subplots_adjust(hspace=0.4, wspace = 0.2, bottom = 0.2)
+    plt.subplots_adjust(hspace=0.4, wspace = 0.4, bottom = 0.2)
 
-    axa.set_ylabel('height / m',fontsize=axis_size)
-    fig.text(0.5, 0.05, 'PAD / m$^2$m$^{-3}$',fontsize=axis_size, ha='center')
-
+    axes[0].set_ylabel('height / m')
+    fig.text(0.5, 0.05, 'PAD / m$^2$m$^{-3}$', ha='center')
     plt.savefig(figure_name)
     plt.show()
     return 0

5ae2a7eff2b047e8eb159de572a05349cf479337
David Milodowski <d.t.milodowski@ed.ac.uk>
2019-09-04 16:03:34 +0100

    individual profile sensitivity and some other more cosmetic changes

--- src/scottish_understory/SEOS_sensitivity_analysis_plots.py
+++ src/scottish_understory/SEOS_sensitivity_analysis_plots.py
@@ -15,7 +15,7 @@ sns.set()
 """
 Plot sensitivity of 1ha profiles to spatial resolution
 """
-def plot_profile_sensitivity_resolution(figure_number,figure_name,heights,PAD_profiles_MH):
+def plot_profile_sensitivity_resolution(figure_number,figure_name,heights,PAD_profiles_profile):
 
     ## Belian
     fig = plt.figure(figure_number, facecolor='White',figsize=[8,4])
@@ -33,17 +33,17 @@ def plot_profile_sensitivity_resolution(figure_number,figure_name,heights,PAD_pr
         ax.annotate(annotations[ii], xy=(0.05,0.95), xycoords='axes fraction',
                     backgroundcolor='none',ha='left', va='top', fontsize=10)
         #rad_sp = np.nanmean(PAD_profiles_rad[keys[ii]],axis=1)
-        MH_sp = np.nanmean(PAD_profiles_MH[keys[ii]],axis=1)
-        MH = np.nanmedian(MH_sp,axis=0)
-        MHperc = np.nanpercentile(MH_sp,[2.5,97.5],axis=0)
-        ax.fill_betweenx(heights[2:],MHperc[0][2:],MHperc[1][2:],color=colour[0],alpha=0.3)
+        profile_sp = np.nanmean(PAD_profiles_profile[keys[ii]],axis=1)
+        profile = np.nanmedian(profile_sp,axis=0)
+        perc = np.nanpercentile(profile_sp,[2.5,97.5],axis=0)
+        ax.fill_betweenx(heights[2:],perc[0][2:],perc[1][2:],color=colour[0],alpha=0.3)
 
         if ii==4:
-            ax.plot(MH[2:],heights[2:],'-',c=colour[0],linewidth=1.5, label = 'MacArthur-Horn')
+            ax.plot(profile[2:],heights[2:],'-',c=colour[0],linewidth=1.5, label = 'MacArthur-Horn')
             #ax.legend(loc=9, bbox_to_anchor=(-0.1, -0.15), ncol=3)
         else:
-            ax.plot(MH[2:],heights[2:],'-',c=colour[0],linewidth=1.5)
-        print('%.3f' % np.sum(MH[2:]))
+            ax.plot(profile[2:],heights[2:],'-',c=colour[0],linewidth=1.5)
+        print('%.3f' % np.sum(profile[2:]))
     yticklabels = axb.get_yticklabels() + axc.get_yticklabels() + axd.get_yticklabels() + axe.get_yticklabels()
     plt.setp(yticklabels,visible=False)
 
@@ -58,47 +58,80 @@ def plot_profile_sensitivity_resolution(figure_number,figure_name,heights,PAD_pr
     axa.set_ylabel('height / m',fontsize=axis_size)
     fig.text(0.5, 0.05, 'PAD / m$^2$m$^{-3}$',fontsize=axis_size, ha='center')
 
-    plt.savefig(figure_name)
-    #plt.show()
+    fig.savefig(figure_name)
+    #fig.show()
     return 0
 
 
-def plot_profile_sensitivity_density(figure_number,figure_name,heights,PAD_profiles_MH):
+def plot_profile_sensitivity_density(figure_name,heights,PAD_profiles):
 
     fig,axes = plt.subplots(1,5,sharey="all",sharex="all",figsize=[8,3])
     annotations = ['20 shots m$^{-2}$','50 shots m$^{-2}$',
                     '100 shots m$^{-2}$','500 shots m$^{-2}$',
                     '1000 shots m$^{-2}$']
-    #axes = [axa,axb,axc,axd,axe]
     # loop through the subplots
     keys = ['20','50','100','500','1000']
     for ii,ax in enumerate(axes):
         ax.set_title(annotations[ii])
-        #rad_sp = np.nanmean(PAD_profiles_rad[keys[ii]],axis=1)
-        MH_sp = np.nanmean(PAD_profiles_MH[keys[ii]],axis=1)
-        MH = np.nanmedian(MH_sp,axis=0)
-        MHperc = np.nanpercentile(MH_sp,[2.5,97.5],axis=0)
-        ax.fill_betweenx(heights[2:],MHperc[0][2:],MHperc[1][2:],color=colour[0],alpha=0.3)
+        profile_sp = np.nanmean(PAD_profiles[keys[ii]],axis=1)
+        profile = np.nanmedian(profile_sp,axis=0)
+        perc = np.nanpercentile(profile_sp,[2.5,97.5],axis=0)
+        ax.fill_betweenx(heights[2:],perc[0][2:],perc[1][2:],color=colour[0],alpha=0.3)
 
         if ii==4:
-            ax.plot(MH[2:],heights[2:],'-',c=colour[0],linewidth=1.5, label = 'MacArthur-Horn')
-            #ax.legend(loc=9, bbox_to_anchor=(-0.1, -0.15), ncol=3)
+            ax.plot(profile[2:],heights[2:],'-',c=colour[0],linewidth=1.5, label = 'MacArthur-Horn')
         else:
-            ax.plot(MH[2:],heights[2:],'-',c=colour[0],linewidth=1.5)
-        print('%.3f' % np.sum(MH[2:]))
-    #yticklabels = axb.get_yticklabels() + axc.get_yticklabels() + axd.get_yticklabels() + axe.get_yticklabels()
-    #plt.setp(yticklabels,visible=False)
+            ax.plot(profile[2:],heights[2:],'-',c=colour[0],linewidth=1.5)
+        print('%.3f' % np.sum(profile[2:]))
 
     axes[0].set_ylim(0,40)
     axes[0].set_xlim(0,0.35)
     for ax in axes:
         ax.locator_params(axis='x',nbins=5)
-        #ax.yaxis.set_ticks_position('both')
 
     plt.subplots_adjust(hspace=0.4, wspace = 0.4, bottom = 0.2)
 
     axes[0].set_ylabel('height / m')
     fig.text(0.5, 0.05, 'PAD / m$^2$m$^{-3}$', ha='center')
-    plt.savefig(figure_name)
-    plt.show()
+    fig.savefig(figure_name)
+    fig.show()
+    return 0
+
+
+def plot_profile_sensitivity_density_individual_profile(figure_name,heights,PAD_profiles, profile_idx=np.nan):
+    if np.isnan(profile_idx):
+        profile_idx = np.random.randint(PAD_profiles['20'].shape[1])
+
+
+    fig,axes = plt.subplots(1,5,sharey="all",sharex="all",figsize=[8,3])
+    annotations = ['20 shots m$^{-2}$','50 shots m$^{-2}$',
+                    '100 shots m$^{-2}$','500 shots m$^{-2}$',
+                    '1000 shots m$^{-2}$']
+    # loop through the subplots
+    keys = ['20','50','100','500','1000']
+    for ii,ax in enumerate(axes):
+        ax.set_title(annotations[ii])
+        profile_sp = PAD_profiles[keys[ii]][:,profile_idx,:]
+        profile = np.nanmedian(profile_sp,axis=0)
+        perc = np.nanpercentile(profile_sp,[2.5,97.5],axis=0)
+        ax.fill_betweenx(heights[2:],perc[0][2:],perc[1][2:],color=colour[0],alpha=0.3)
+
+        if ii==4:
+            ax.plot(profile[2:],heights[2:],'-',c=colour[0],linewidth=1.5, label = 'MacArthur-Horn')
+            #ax.legend(loc=9, bbox_to_anchor=(-0.1, -0.15), ncol=3)
+        else:
+            ax.plot(profile[2:],heights[2:],'-',c=colour[0],linewidth=1.5)
+        print('%.3f' % np.sum(profile[2:]))
+
+    axes[0].set_ylim(0,40)
+    axes[0].set_xlim(0,1.05)
+    for ax in axes:
+        ax.locator_params(axis='x',nbins=3)
+
+    plt.subplots_adjust(hspace=0.4, wspace = 0.4, bottom = 0.2)
+
+    axes[0].set_ylabel('height / m')
+    fig.text(0.5, 0.05, 'PAD / m$^2$m$^{-3}$', ha='center')
+    fig.savefig(figure_name)
+    fig.show()
     return 0
